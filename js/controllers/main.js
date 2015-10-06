9bee1f30106459bb0f3525ed5a6aa910955272bb
Carlos Soltero <carlosh95@gmail.com>
2015-10-06 02:00:48 -0400

    adding skilss icons

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -3,4 +3,46 @@ myPortfolio.controller('MainCtrl', function($scope){
 		$scope.heroImage = {
         'background-image': 'url(img/backDev.jpg)'
 	    };
+	    $scope.profilePic = 'img/profileGA.jpg';
+	    $scope.icons = [
+	    	{
+	    		"name": "js.png"
+	    	},
+	    	{
+	    		"name": "html.png"
+	    	},
+	    	{
+	    		"name": "css3.png"
+	    	},
+	    	{
+	    		"name": "json.png"
+	    	},
+	    	{
+	    		"name": "angular.png"
+	    	},
+	    	{
+	    		"name": "rails.png"
+	    	},
+	    	{
+	    		"name": "git.png"
+	    	},
+	    	{
+	    		"name": "grunt.png"
+	    	},
+	    	{
+	    		"name": "sql.png"
+	    	},
+	    	{
+	    		"name": "bootstrap.png"
+	    	},
+	    	{
+	    		"name": "ps.png"
+	    	},
+	    	{
+	    		"name": "node.png"
+	    	// },
+	    	// {
+	    	// 	"name": "mongo.png"
+	    	}
+	    ];
 	})
