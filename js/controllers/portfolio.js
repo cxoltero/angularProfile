50ddc14f5ab2e22f3ad41c43f59ab6a6e8e4ff2d
Carlos Soltero <carlosh95@gmail.com>
2015-10-08 23:54:23 -0400

    fixed bootstrap ng-repeat issue

--- js/controllers/portfolio.js
+++ js/controllers/portfolio.js
@@ -1,14 +1,6 @@
 'use strict'
 myPortfolio.controller('PortfolioCtrl', function($scope){
-		var path = 'img/';
 
-		// $scope.pixelect = path+'pixelect.png';
-		// $scope.easyManage = path + 'easyManage.png';
-		// $scope.angularContacts = path+'angularContacts.png';
-		// $scope.youSearch = path+ 'youSearch.png';
-		// $scope.imgGallery = path+'imgGallery.png';
-		// $scope.mexRestaurant = path+'mexRestaurant.png';
-		// $scope.galleryFront = path+ 'galleryFront.png';
 		$scope.projects = [
 			{
 				'name': 'Pixelect',
